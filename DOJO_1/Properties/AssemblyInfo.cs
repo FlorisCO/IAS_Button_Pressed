f9c98b93c9fe9448bb16d74cb8de185a8f486d00
Floris Cockaerts <floris.cockaerts@skyline.be>
2023-06-12 16:36:40 +0200

    Add project files.

---
+++ DOJO_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DOJO_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("DOJO_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("7C478D05-B88F-4CDA-8B78-A2886124FBE0")]
+[assembly: AssemblyVersion("1.0.0.0")]
