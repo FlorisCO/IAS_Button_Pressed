d26fb415175723493b3be76846a5884ac2c2dff7
Floris Cockaerts <floris.cockaerts@skyline.be>
2023-07-04 09:47:52 +0200

    Added DisabledWhileProcessing
    Showcase how to change the view state while processing data in an event handler. Also works for a low-code app.

---
+++ DisabledWhileProcessing_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DisabledWhileProcessing_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("DisabledWhileProcessing_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("C0E572CA-804B-4E86-8E51-D364AFFD37D9")]
+[assembly: AssemblyVersion("1.0.0.0")]
